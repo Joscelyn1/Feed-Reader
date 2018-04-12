b5280245f4a2e571f855b287a493121925580b19
Joscelyn <JoscelynRose@gmail.com>
2018-04-11 17:21:57 -0700

    added comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -18,21 +18,21 @@ $(function() {
     describe('RSS Feeds', function() {
 
         it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds).toBeDefined(); //expects allFeeds to not be undefined
+            expect(allFeeds.length).not.toBe(0); //expects its length to not be 0
         });
 
         it('have URLs', function() {
-            for (let i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].url).toBeDefined();
-                expect(allFeeds[i].url).not.toBe("");
+            for (let i = 0; i < allFeeds.length; i++) { // loops through the allFeeds array
+                expect(allFeeds[i].url).toBeDefined(); // makes sure each element's url property is defined
+                expect(allFeeds[i].url).not.toBe(''); // makes sure each element's url property is not an empty string
             }
         });
 
         it('have names', function() {
-            for (let i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].name).toBeDefined();
-                expect(allFeeds[i].name).not.toBe('');
+            for (let i = 0; i < allFeeds.length; i++) { // loops through the allFeeds array
+                expect(allFeeds[i].name).toBeDefined(); // makes sure each element's name property is defined
+                expect(allFeeds[i].name).not.toBe(''); // makes sure each element's name property is not an empty string
             }
         });
     });
@@ -42,15 +42,15 @@ $(function() {
        const body = document.querySelector('body');
 
        it('is hidden by default', function() {
-            expect(body.classList.contains('menu-hidden')).toBe(true);
+            expect(body.classList.contains('menu-hidden')).toBe(true); //expect the body to have the "menu-hidden" class
         });
 
        it('changes visibility when clicked', function() {
-            var menuIcon = document.querySelector(".menu-icon-link");
-            menuIcon.click();
-            expect(body.classList.contains('menu-hidden')).toBe(false);
-            menuIcon.click();
-            expect(body.classList.contains('menu-hidden')).toBe(true);
+            var menuIcon = document.querySelector('.menu-icon-link');
+            menuIcon.click(); // clicks the menu icon
+            expect(body.classList.contains('menu-hidden')).toBe(false); // expects the menu to be visible
+            menuIcon.click(); // clicks hte menu icon
+            expect(body.classList.contains('menu-hidden')).toBe(true); // expects the menu to be hidden
         });
     });
 
@@ -62,7 +62,7 @@ $(function() {
 
         it('have at least one entry', function(done) {
             const listOfEntries = document.querySelectorAll('.feed .entry');
-            expect(listOfEntries.length).toBeGreaterThan(0);
+            expect(listOfEntries.length).toBeGreaterThan(0); // expects there to be at least 1 entry class
             done();
         });
     });
@@ -71,17 +71,17 @@ $(function() {
 
         beforeEach(function() {
             loadFeed(0, function(done) {
-                entryItems1 = document.querySelector('.entry').innerHTML;
+                entryItems1 = document.querySelector('.entry').innerHTML; //after the feed is loaded at index 0, saves the html
 
-                loadFeed(1, function(done) {
+                loadFeed(1, function(done) { // loads the feed at index 1
                     done();
                 })
             });
         });
 
         it('updates the feed', function(done) {
-            entryItems2 = document.querySelector('.entry').innerHTML;
-            expect(entryItems1 === entryItems2).toBe(false);
+            entryItems2 = document.querySelector('.entry').innerHTML; // after the feed is loaded at index 1, saves the html
+            expect(entryItems1 === entryItems2).toBe(false); // compares entryItems1 and entryItems2, expects them to be different
             done();
         });
     });
